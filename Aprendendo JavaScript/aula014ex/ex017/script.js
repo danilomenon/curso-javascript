2b33de7aa4139740c90ed21388fd5fd0cacffc37
Danilo Menon <menondanilo153@gmail.com>
2024-12-12 09:31:59 -0300

    updates

--- Aprendendo JavaScript/aula014ex/ex017/script.js
+++ Aprendendo JavaScript/aula014ex/ex017/script.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+function tabuada() {
+    var tab = 1
+    var c = 0
+
+    while(c <= 10) {
+        var result = tab * c
+        console.log(`${tab} x ${c} = ${result}`)
+        c++
+    }
+}
+
+tabuada()
