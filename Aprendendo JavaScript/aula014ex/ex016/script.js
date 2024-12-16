d807f38e056e0d2532981b6f3487a1963a84a4a0
Danilo Menon <menondanilo153@gmail.com>
2024-12-16 13:28:45 -0300

    Lógica em andamento...

--- Aprendendo JavaScript/aula014ex/ex016/script.js
+++ Aprendendo JavaScript/aula014ex/ex016/script.js
@@ -1,14 +1,9 @@
 function contar() {
-    var n1 = document.getElementById('txtn1').value
+   var n1 = document.getElementById('txtn1').value
 
     var n2 = document.getElementById('txtn2').value
 
-    var res = document.getElementById('res')
-
-    while (n1 <= n2) {
-        res.innerText = `${res}`
-        n1++
-    }
+    var p = document.getElementById('passo')
 
 
 }
