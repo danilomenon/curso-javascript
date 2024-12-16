145b3520d1763b66b894932aaa8f9cd63b36fd09
Danilo Menon <menondanilo153@gmail.com>
2024-12-16 13:15:59 -0300

    Exercicio pendente...

--- Aprendendo JavaScript/aula014ex/ex016/script.js
+++ Aprendendo JavaScript/aula014ex/ex016/script.js
@@ -1,1 +1,14 @@
-alert('Hello world!')
+function contar() {
+    var n1 = document.getElementById('txtn1').value
+
+    var n2 = document.getElementById('txtn2').value
+
+    var res = document.getElementById('res')
+
+    while (n1 <= n2) {
+        res.innerText = `${res}`
+        n1++
+    }
+
+
+}
