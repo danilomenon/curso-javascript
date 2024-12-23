271aedb28598634b9a474d5785475adfc5d2f0a8
Danilo Menon <menondanilo153@gmail.com>
2024-12-23 13:39:46 -0300

    Resoluçao do exercicio quinze, concluído!

--- Aprendendo JavaScript/aula014ex/ex016/script.js
+++ Aprendendo JavaScript/aula014ex/ex016/script.js
@@ -1,11 +1,35 @@
 function contar() {
-   var n1 = document.getElementById('txtn1').value
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    var n2 = document.getElementById('txtn2').value
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        // window.alert('[ERRO] Faltam dados')
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
 
-    var p = document.getElementById('passo').value
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
 
-    var res = document.getElementById('res')
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            }
 
-    res.innerHTML = `${n1}, ${n2} e ${p}`
+        } else {
+            // Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
