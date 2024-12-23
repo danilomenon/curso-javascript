3449efa2e66a8f25f8c05e6eb337f7198b2841ad
Danilo Menon <menondanilo153@gmail.com>
2024-12-23 14:08:18 -0300

    Desafio da Tabuada - Concluída

--- Aprendendo JavaScript/aula014ex/ex017/script.js
+++ Aprendendo JavaScript/aula014ex/ex017/script.js
@@ -1,12 +1,28 @@
-function myTable() {
-    var c = 1
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+    /*var c = 1
     var tab = 2
 
     while (c <= 10) {
         var res = c * tab
         console.log(`${tab} x ${c} = ${res}`);
         c++
-    }
+    }*/
 }
-
-myTable()
